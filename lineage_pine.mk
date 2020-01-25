7d97bbecb053b6ee2becc727e4b1817aa03b04b9
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-01-25 18:28:47 +0100

    Lineage-ify

---
+++ lineage_pine.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from pine device
+$(call inherit-product, device/sony/pine/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := SONY
+PRODUCT_DEVICE := pine
+PRODUCT_NAME := lineage_pine
+PRODUCT_BRAND := SONY
+PRODUCT_MODEL := G3311
+PRODUCT_MANUFACTURER := SONY
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=pine
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
