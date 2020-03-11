2939ae6788ddd12da734819f54743cafd5554cee
rote66 <pontifex23872388@gmail.com>
2020-03-12 01:40:30 +0800

    patch update

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -9,6 +9,7 @@ patch -p1 < ../../device/sony/pine/patches/frameworks/av.patch
 patch -p1 < ../../device/sony/pine/patches/frameworks/0006-fix-access-wvm-to-ReadOptions.patch
 patch -p1 < ../../device/sony/pine/patches/frameworks/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
 patch -p1 < ../../device/sony/pine/patches/frameworks/frameworks-av_016-fix-cannot-locate-symbol.patch
+patch -p1 < ../../device/sony/pine/patches/frameworks/0009-add-mtk-color-format-support.patch
 cd ../..
 cd frameworks/base
 patch -p1 < ../../device/sony/pine/patches/frameworks/base.patch
