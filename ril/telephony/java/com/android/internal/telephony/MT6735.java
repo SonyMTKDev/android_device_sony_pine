716cec1d49a48ae65f09ff477b331e119192fc86
rote66 <pontifex23872388@gmail.com>
2020-03-12 19:57:24 +0800

    revert ril java

--- ril/telephony/java/com/android/internal/telephony/MT6735.java
+++ ril/telephony/java/com/android/internal/telephony/MT6735.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,24 @@
 package com.android.internal.telephony;
 
 import static com.android.internal.telephony.RILConstants.*;
+
 import com.android.internal.telephony.dataconnection.DataCallResponse;
 import com.android.internal.telephony.uicc.IccRefreshResponse;
+
 import android.content.Context;
 import android.os.AsyncResult;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.SystemProperties;
+
 import android.provider.Settings;
 import android.provider.Settings.Global;
+
+import android.telephony.Rlog;
 import android.telephony.TelephonyManager;
+
 import com.android.internal.telephony.MtkEccList;
+
 
 /**
  * Custom wrapper for MTK requests
@@ -41,6 +48,7 @@ public class MT6735 extends RIL implements CommandsInterface {
     private static final int RIL_UNSOL_INCOMING_CALL_INDICATION = 3042;
     private static final int RIL_UNSOL_CALL_INFO_INDICATION = 3049;
     private static final int RIL_UNSOL_SET_ATTACH_APN = 3073;
+
     private static final int RIL_REQUEST_MODEM_POWEROFF = 2010;
     private static final int RIL_REQUEST_MODEM_POWERON = 2028;
     private static final int RIL_REQUEST_RESUME_REGISTRATION = 2065;
@@ -55,10 +63,12 @@ public class MT6735 extends RIL implements CommandsInterface {
     //private Context mContext;
     private TelephonyManager mTelephonyManager;
     private MtkEccList mEccList;
+    
 
-    public MT6735(Context context, int preferredNetworkType, int cdmaSubscription) {
+   public MT6735(Context context, int preferredNetworkType, int cdmaSubscription) {
         super(context, preferredNetworkType, cdmaSubscription, null);
         //mContext = context;
+        Rlog.i("MT6735", "Ctor1: context is " + mContext);
         mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         mEccList = new MtkEccList();
     }
@@ -67,6 +77,7 @@ public class MT6735 extends RIL implements CommandsInterface {
             int cdmaSubscription, Integer instanceId) {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
         //mContext = context;
+        Rlog.i("MT6735", "Ctor2: context is " + mContext);
         mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         mEccList = new MtkEccList();
     }
@@ -85,6 +96,7 @@ public class MT6735 extends RIL implements CommandsInterface {
             default: return "<unknown response>";
         }
     }
+
 
     @Override
     protected void
@@ -221,6 +233,7 @@ public class MT6735 extends RIL implements CommandsInterface {
         rr.mParcel.writeInt(authType);
         rr.mParcel.writeString(username);
         rr.mParcel.writeString(password);
+
         rr.mParcel.writeString(operatorNumber);
         rr.mParcel.writeInt(1);
         rr.mParcel.writeStringArray(null);
@@ -237,7 +250,7 @@ public class MT6735 extends RIL implements CommandsInterface {
     responseSetAttachApn(Parcel p) {
         // The stack refuses to attach to LTE unless an IA APN was set, and
         // will loop until it happens. Set an empty one to unblock.
-        setInitialAttachApn("","",0,"","",null);
+       setInitialAttachApn("","",0,"","",null);        
         return null;
     }
 
@@ -257,7 +270,7 @@ public class MT6735 extends RIL implements CommandsInterface {
                 response.refreshResult = IccRefreshResponse.REFRESH_RESULT_INIT;
             }
         }
-		
+
         return response;
     }
 
@@ -343,7 +356,6 @@ public class MT6735 extends RIL implements CommandsInterface {
             if (RILJ_LOGD) riljLog(rr.serialString() + "> " + localRequestToString(rr.mRequest));
 
             send(rr);
-
         } else {
             super.dial(address, clirMode, uusInfo, result);
         }
@@ -380,7 +392,7 @@ public class MT6735 extends RIL implements CommandsInterface {
             super.setRadioPower(on, result);
         }
     }
-
+        
     // Solicited request handling
     @Override
     protected RILRequest
@@ -436,7 +448,7 @@ public class MT6735 extends RIL implements CommandsInterface {
         }
 
         Object ret = null;
-
+        
         if (error == 0 || p.dataAvail() > 0) {
             switch (rr.mRequest) {
                 case RIL_REQUEST_EMERGENCY_DIAL: ret =  responseVoid(p); break;
@@ -498,9 +510,10 @@ public class MT6735 extends RIL implements CommandsInterface {
     iccIOForApp (int command, int fileid, String path, int p1, int p2, int p3,
             String data, String pin2, String aid, Message result) {
         if (command == 0xc0 && p3 == 0) {
+            Rlog.i("MT6735", "Override the size for the COMMAND_GET_RESPONSE 0 => 15");
             p3 = 15;
         }
         super.iccIOForApp(command, fileid, path, p1, p2, p3, data, pin2, aid, result);
     }
-
+   
 }
