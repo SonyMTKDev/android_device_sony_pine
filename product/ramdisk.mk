a925684c3aabef054317d03b72afa40ff09817f2
rote66 <pontifex23872388@gmail.com>
2020-03-13 00:45:14 +0800

    RC:add some rc from pine

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -25,6 +25,17 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/etc/init/cameraserver.rc:system/etc/init/cameraserver.rc \
     $(DEVICE_PATH)/etc/init/rild.rc:system/etc/init/rild.rc 
 
+#Rc parts add for pine
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/etc/init/atrace.rc:system/etc/init/atrace.rc \
+    $(DEVICE_PATH)/etc/init/bootanim.rc:system/etc/init/bootanim.rc \
+    $(DEVICE_PATH)/etc/init/bootstat.rc:system/etc/init/bootstat.rc \
+    $(DEVICE_PATH)/etc/init/drmserver.rc:system/etc/init/drmserver.rc \
+    $(DEVICE_PATH)/etc/init/mediacodec.rc:system/etc/init/mediacodec.rc \
+    $(DEVICE_PATH)/etc/init/mediadrmserver.rc:system/etc/init/mediadrmserver.rc \
+    $(DEVICE_PATH)/etc/init/mediaserver.rc:system/etc/init/mediaserver.rc \
+    $(DEVICE_PATH)/etc/init/surfaceflinger.rc:system/etc/init/surfaceflinger.rc \
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/sony/pine/prebuilts/kernel
 else
