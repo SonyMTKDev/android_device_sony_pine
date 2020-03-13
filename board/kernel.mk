a4978d444b25933eb16a73a7c7a73c40bd50c50b
jmpfbmx <jmpf.bmx@gmail.com>
2020-03-13 16:11:41 +0100

    pine: Don't use prebuilt kernel

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,18 +1,14 @@
 # Kernel
 TARGET_KERNEL_ARCH := arm64
-#TARGET_KERNEL_HEADER_ARCH := arm64
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#TARGET_KERNEL_CONFIG := pine_defconfig
-#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-#TARGET_KERNEL_SOURCE := kernel/sony/pine
-
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+TARGET_KERNEL_CONFIG := pine_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/sony/pine
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-#BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=disabled
-BOARD_KERNEL_OFFSET = 0x00080000
-BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_BASE := 0x40078000
+BOARD_RAMDISK_OFFSET := 0x03f88000
+
+BOARD_NO_SECURE_DISCARD := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00000000 --tags_offset 0x0df88000 --base 0x40078000
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
-BOARD_FLASH_BLOCK_SIZE := 131072
-#BOARD_RAMDISK_OFFSET := 0x03f88000
