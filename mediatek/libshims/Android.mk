0107b8259432e84b9e7516b0fc4980d761a1eaa6
jmpfbmx <jmpf.bmx@gmail.com>
2020-10-14 21:54:36 +0200

    pine: Label vt_shim

--- mediatek/libshims/Android.mk
+++ mediatek/libshims/Android.mk
@@ -41,6 +41,7 @@ LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
+# Mal Shim (vt)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
