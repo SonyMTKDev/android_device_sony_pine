f3635e62813982dba92b33f9217619ee9ed580ef
fire855 <thefire855@gmail.com>
2021-01-30 16:19:56 +0100

    pine: Update liblights

--- mediatek/liblights/Android.mk
+++ mediatek/liblights/Android.mk
@@ -17,18 +17,17 @@ LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
-LOCAL_HEADER_LIBRARIES := libhardware_headers
 
 LOCAL_SRC_FILES := lights.c
 LOCAL_MULTILIB := both
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
+# LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_RELATIVE_PATH := hw
-
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
-LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
