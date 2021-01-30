c8cea99408b552a09508a9e8e5e0de0dc61d71d1
rote66 <pontifex23872388@gmail.com>
2021-01-30 10:51:24 +0100

    pine: BoardConfig: Add missing CPPFLAGS and CFLAGS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,8 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_USES_64_BIT_BINDER := true
 
 # Architecture Extensions
