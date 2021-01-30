c089ca0b9d613bf6fd4d8f790111acaaba727076
rote66 <pontifex23872388@gmail.com>
2021-01-30 13:19:58 +0100

    pine: Drop TARGET_USE_CUSTOM_LUN_FILE_PATH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,9 +130,6 @@ TARGET_OTA_ASSERT_DEVICE := pine,Sony_L1
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp-policy
 BOARD_SEPOLICY_DIRS := $(DEVICE_PATH)/sepolicy
 
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
-
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
