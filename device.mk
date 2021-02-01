f06a47f4e45637ffd4a61ac01573719f68d7dbf9
rote66 <pontifex23872388@gmail.com>
2021-02-01 10:14:30 +0100

    pine: device: Set persist.sys.dun.override as 0

--- device.mk
+++ device.mk
@@ -158,6 +158,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1 \
     ro.oem_unlock_supported=1 \
     persist.service.acm.enable=0 \
+    persist.sys.dun.override=0 \
     sys.usb.ffs.aio_compat=1 \
     ro.mount.fs=EXT4 \
     ro.mtk_key_manager_kb_path=1 
