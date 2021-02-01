11f3c11e13911e466605a723cd76cc4bc9cf6b69
Maanush Putcha <p.maanush@gmail.com>
2021-02-01 10:09:21 +0100

    pine: device: Set ro.telephony.ril.config as fakeiccid

--- device.mk
+++ device.mk
@@ -74,7 +74,8 @@ PRODUCT_PACKAGES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
-    ro.telephony.ril_class=MT6735
+    ro.telephony.ril_class=MT6735 \
+    ro.telephony.ril.config=fakeiccid
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
