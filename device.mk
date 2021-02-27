ce01a35e011e7fb2e01be7a7ac7cc96eb51ef928
xcore995 <arman.p95@gmail.com>
2021-02-27 17:17:30 +0100

    pine: switch to legacy camera HAL

--- device.mk
+++ device.mk
@@ -81,10 +81,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl-legacy
 
 # Network
 PRODUCT_PACKAGES += \
