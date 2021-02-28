db4eb8e90dcf62833ba63c6834b54db5a8ea6254
R0rt1z2 <rogerortizleal@gmail.com>
2021-02-28 15:34:04 +0100

    pine: libshims: gui: include string header
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- mediatek/libshims/gui/gui_shim.cpp
+++ mediatek/libshims/gui/gui_shim.cpp
@@ -5,6 +5,7 @@
 #include <ui/Rect.h>
 #include <log/log.h>
 #include <dlfcn.h>
+#include <string>
 
 extern "C" {
     void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(buffer_handle_t, uint32_t, const android::Rect&, android_ycbcr*);
