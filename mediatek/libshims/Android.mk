1d4cbf1f927174e4c956555d34d369102a79c9eb
R0rt1z2 <rogerortizleal@gmail.com>
2021-02-28 17:09:29 +0100

    pine: libshims: remove duplicated entry
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- mediatek/libshims/Android.mk
+++ mediatek/libshims/Android.mk
@@ -44,21 +44,6 @@ LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
-# Mal Shim (vt)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    vt/vt_shim.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils
-
-LOCAL_MODULE := libshim_vt
-
-LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
 # XLOG Shim
 include $(CLEAR_VARS)
 
