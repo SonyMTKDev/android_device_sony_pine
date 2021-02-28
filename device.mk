189c368458bfc2b69be350f55787416d6f0ca236
KakatkarAkshay <cryllicbuster273@pixelexperience.org>
2021-02-28 17:09:00 +0100

    pine: build thermal HAL from source

--- device.mk
+++ device.mk
@@ -173,6 +173,11 @@ PRODUCT_PACKAGES += \
     librs_jni \
     android.hardware.usb@1.0-service
 
+# Thermal
+PRODUCT_PACKAGES += \
+	android.hardware.thermal@1.0-impl \
+	android.hardware.thermal@1.0-service
+
 # Override Default Properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1 \
