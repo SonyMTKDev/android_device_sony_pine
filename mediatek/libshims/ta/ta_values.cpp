18e67905e18dc6d9927af1dab5f5cdeca8aee1dd
R0rt1z2 <rogerortizleal@gmail.com>
2021-06-28 11:21:15 +0200

    pine: libshims: ta: Fix building
     * At the original commit * was missing
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I094dd13ca78413686e643c6c360fd34578f89f4e

--- mediatek/libshims/ta/ta_values.cpp
+++ mediatek/libshims/ta/ta_values.cpp
@@ -5,15 +5,15 @@
 #include "ta_values.h"
 
 namespace android {
-    const char miscta_read_::ta_cust_version = "2205";
-    const char miscta_read_::ta_fla_fla = "2202";
-    const char miscta_read_::ta_fs_version = "2206";
-    const char miscta_read_::ta_phone_name = "2210";
-    const char miscta_read_::ta_sw_version = "2202";
-    const char miscta_write_::ta_build_type = "2209";
-    const char miscta_write_::ta_cust_version = "2205";
-    const char miscta_write_::ta_fla_fla = "2202";
-    const char miscta_write_::ta_fs_version = "2206";
-    const char miscta_write_::ta_phone_name = "2210";
-    const char miscta_write_::ta_sw_version = "2202";
+    const char *miscta_read_::ta_cust_version = "2205";
+    const char *miscta_read_::ta_fla_fla = "2202";
+    const char *miscta_read_::ta_fs_version = "2206";
+    const char *miscta_read_::ta_phone_name = "2210";
+    const char *miscta_read_::ta_sw_version = "2202";
+    const char *miscta_write_::ta_build_type = "2209";
+    const char *miscta_write_::ta_cust_version = "2205";
+    const char *miscta_write_::ta_fla_fla = "2202";
+    const char *miscta_write_::ta_fs_version = "2206";
+    const char *miscta_write_::ta_phone_name = "2210";
+    const char *miscta_write_::ta_sw_version = "2202";
 }
--- mediatek/libshims/ta/ta_values.h
+++ mediatek/libshims/ta/ta_values.h
@@ -6,20 +6,20 @@ namespace android {
     /* MiscTA Read Start */
     class miscta_read_ {
         public:
-            static const char ta_cust_version;
-            static const char ta_fla_fla;
-            static const char ta_fs_version;
-            static const char ta_phone_name;
-            static const char ta_sw_version;
+            static const char *ta_cust_version;
+            static const char *ta_fla_fla;
+            static const char *ta_fs_version;
+            static const char *ta_phone_name;
+            static const char *ta_sw_version;
     };
     /* MiscTA Write Start */
     class miscta_write_ {
         public:
-            static const char ta_build_type;
-            static const char ta_cust_version;
-            static const char ta_fla_fla;
-            static const char ta_fs_version;
-            static const char ta_phone_name;
-            static const char ta_sw_version;
+            static const char *ta_build_type;
+            static const char *ta_cust_version;
+            static const char *ta_fla_fla;
+            static const char *ta_fs_version;
+            static const char *ta_phone_name;
+            static const char *ta_sw_version;
     };
 };
